c773c8c315c9c1d557054b35f17c059fb392acfb
kyznano <kyznano@gmail.com>
2014-04-02 13:02:55 +0700

    Update calculate ( ), +, -, *, /

--- Project/SmartCalculator/SmartCalculator/Cacualate/Calculate.cs
+++ Project/SmartCalculator/SmartCalculator/Cacualate/Calculate.cs
@@ -10,11 +10,14 @@ namespace SmartCalculator.Cacualate
     {
         String formular;
         String errorString;
+        String backup_input;
+        List<String> components = new List<String>();
 
         //constructor
         public Calculate(String seq)
         {
             formular = seq;
+            backup_input = seq.Replace("_", "");
         }
 
         public String ErrorString
@@ -32,7 +35,85 @@ namespace SmartCalculator.Cacualate
 
         //You can modify all things, but you need add the result to this function
         public String CalResult(){
-            return "1";
+            return CalComponent(backup_input);
+        }
+
+        public String CalComponent(String i)
+        {
+            String s = i;
+            bool o = true;
+            while (o)
+            {
+                String res = ExtractComponents(s);
+                if (res == null) o = false;
+                else
+                {
+                    String new_res = CalComponent(res);
+                    s = s.Replace('(' + res + ')', new_res);
+                }
+            }
+            String de = "+-*/";
+            char[] delimiter = de.ToCharArray();
+            if(s.Contains('+'))
+            {
+                String[] spl = s.Split('+');
+                float res = 0;
+                foreach(String addend in spl)
+                {
+                    //s = s.Replace(left + '+' + right, float(CalComponent(left)) + float(CalComponent(right)));
+                    res += float.Parse(CalComponent(addend));
+                }
+                s = res.ToString();
+                //return "0";
+            } else if(s.Contains('-'))
+            {
+                String[] spl = s.Split('-');
+                float res = 0;
+                bool first = true;
+                foreach(String addend in spl)
+                {
+                    //s = s.Replace(left + '+' + right, float(CalComponent(left)) + float(CalComponent(right)));
+                    if (first) { res = float.Parse(CalComponent(addend)); first = false; }
+                    else res -= float.Parse(CalComponent(addend));
+                }
+                s = res.ToString();
+            }
+            else if (s.Contains('*'))
+            {
+                String[] spl = s.Split('*');
+                float res = 1;
+                foreach (String addend in spl)
+                {
+                    //s = s.Replace(left + '+' + right, float(CalComponent(left)) + float(CalComponent(right)));
+                    res = res*float.Parse(CalComponent(addend));
+                }
+                s = res.ToString();
+            }
+            else if (s.Contains('/'))
+            {
+                String[] spl = s.Split('/');
+                float res = 0;
+                bool first = true;
+                foreach (String addend in spl)
+                {
+                    //s = s.Replace(left + '+' + right, float(CalComponent(left)) + float(CalComponent(right)));
+                    if (first) { res = float.Parse(CalComponent(addend)); first = false; }
+                    else res = res / float.Parse(CalComponent(addend));                    
+                }
+                s = res.ToString();
+            }
+            return s;
+        }
+
+        public String ExtractComponents(String i)
+        {
+            if (i.Contains(')'))
+            {
+                    String tail = i.Split(')')[0];
+                    String[] tail_split = tail.Split('(');
+                    return tail_split[tail_split.Length-1];
+            }
+            return null;
         }
     }
 }
